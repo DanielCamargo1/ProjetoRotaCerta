a61354f8cac792218cbb646b5f502a06cec87b62
DanielCamargo <danidcp098@gmail.com>
2024-10-03 11:07:43 -0300

    project root

---
+++ js/dom.js
@@ -0,0 +1,15 @@
+﻿function IniciarViagem() {
+    const inputGasolina = document.getElementById("gasolinaInicial");
+    const finalContainer = document.getElementById("stop");
+    const buttonFinalizar = document.getElementById("buttonFinal");
+
+
+    let value = parseFloat(inputGasolina.value);
+
+    if (isNaN(value) || value <= 0) {
+        alert("É necessário informar um valor válido para gasolina.");
+    } else {
+        finalContainer.style.display = "block";
+        buttonFinalizar.style.display = "block";
+    }
+}
---
+++ js/map.js
@@ -0,0 +1,59 @@
+﻿var map;
+var coordenadas = []; // Array das coordenadas
+var watchId; // Armazena o ID do setInterval
+
+function success(pos) {
+    const latitude = pos.coords.latitude;
+    const longitude = pos.coords.longitude;
+
+    // Adiciona ao array
+    coordenadas.push({ latitude: latitude, longitude: longitude });
+
+    if (map === undefined) {
+        map = L.map('map').setView([latitude, longitude], 13);
+    } else {
+        map.remove();
+        map = L.map('map').setView([latitude, longitude], 13);
+    }
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([latitude, longitude]).addTo(map)
+        .bindPopup('Minha Localização')
+        .openPopup();
+}
+
+function FinalizarViagem() {
+    // Parar o intervalo
+    clearInterval(watchId);
+    alert("Viagem finalizada. Parando a obtenção da localização.");
+
+    const inputGasolinaFinal = document.getElementById("gasolinaInicial");
+    /* pegar o destino final*/
+
+    const markerFinal = L.marker(coordenadas[coordenadas.length - 1])
+    markerFinal.addTo(map);
+    const polyline = L.polyline(coordenadas, { color: '#F00' })
+    polyline.addTo(map)
+  
+    /* calcullo da media */
+}
+
+function error(err) {
+    console.log(err);
+}
+
+// Obter localização a cada 3 segundos
+function iniciarRastreamento() {
+    watchId = setInterval(function () {
+        navigator.geolocation.getCurrentPosition(success, error, {
+            enableHighAccuracy: true,
+            timeout: 5000
+        });
+    }, 3000);
+}
+
+
+iniciarRastreamento();
